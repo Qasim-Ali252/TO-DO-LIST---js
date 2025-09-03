6254d51af2d5dfd4d717e5f12dd5af15b342907c
QasimAli-252 <qasimmunir633@gmail.com>
2025-09-04 02:53:27 +0500

    added To-do list project

--- filteroddeven.js
+++
@@ -1,8 +0,0 @@
-let arr = [1,2,3,4,5,6,7,8,9,10];
-
-let even = arr.filter((num) => {return num %2 ===0});
-let odd = arr.filter((num) => {return num %2 !==0});
-
-console.log(even);
-console.log(odd);
-
